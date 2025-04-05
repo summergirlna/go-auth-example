5f704cd0867a5ab8e44acd029c57750d001809d2
kurita <summergirlna@gmail.com>
2025-04-05 23:07:52 +0900

    ログイン処理追加

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -8,6 +8,36 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+
+	// *************************************
+	// ログイン(初期化)
+	// *************************************
+	router.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.tmpl", gin.H{})
+	})
+
+	// *************************************
+	// ログイン(認証)
+	// *************************************
+	router.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		// *************************************
+		// 認証成功の場合
+		// *************************************
+		if username == "admin" && password == "admin" {
+			c.Redirect(http.StatusFound, "/index")
+		} else {
+			c.HTML(http.StatusUnauthorized, "login.tmpl", gin.H{
+				"error": "認証情報に誤りがあります。",
+			})
+		}
+	})
+
+	// *************************************
+	// メインページ(ダミー)
+	// *************************************
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
