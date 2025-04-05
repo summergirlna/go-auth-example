0251425220a5c8a26aa9354f107cab6a13306199
kurita <summergirlna@gmail.com>
2025-04-06 00:00:27 +0900

    LDAP処理

--- cmd/app1/main.go
+++ cmd/app1/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-ldap/ldap/v3"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -23,16 +26,65 @@ func main() {
 		username := c.PostForm("username")
 		password := c.PostForm("password")
 
-		// *************************************
-		// 認証成功の場合
-		// *************************************
-		if username == "admin" && password == "admin" {
-			c.Redirect(http.StatusFound, "/index")
-		} else {
+		// 以下環境変数は、「localhost:389」のように指定する
+		ldapURL := os.Getenv("LDAP_URL")
+
+		// LDAP接続
+		conn, err := ldap.DialURL(ldapURL)
+		if err != nil {
+			c.HTML(http.StatusInternalServerError, "login.tmpl", gin.H{
+				"error": fmt.Sprintf("LDAPサーバ接続に失敗しました。詳細: %s\n", err.Error()),
+			})
+			return
+		}
+		defer conn.Close()
+
+		baseDN := "dc=umiyama,dc=com"
+
+		// 管理者バインド
+		adminDN := fmt.Sprintf("cn=admin,%s", baseDN)
+		err = conn.Bind(adminDN, "admin")
+		if err != nil {
+			c.HTML(http.StatusInternalServerError, "login.tmpl", gin.H{
+				"error": fmt.Sprintf("管理者バインドに失敗しました。詳細: %s\n", err.Error()),
+			})
+			return
+		}
+
+		// サブツリー検索
+		searchRequest := ldap.NewSearchRequest(
+			baseDN,
+			ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+			fmt.Sprintf("(uid=%s)", username),
+			[]string{"dn"},
+			nil,
+		)
+		sr, err := conn.Search(searchRequest)
+		if err != nil {
 			c.HTML(http.StatusUnauthorized, "login.tmpl", gin.H{
 				"error": "認証情報に誤りがあります。",
 			})
 		}
+
+		if len(sr.Entries) != 1 {
+			c.HTML(http.StatusUnauthorized, "login.tmpl", gin.H{
+				"error": "認証情報に誤りがあります。",
+			})
+		}
+
+		userDN := sr.Entries[0].DN
+
+		// バインド
+		err = conn.Bind(userDN, password)
+		if err != nil {
+			c.HTML(http.StatusUnauthorized, "login.tmpl", gin.H{
+				"error": "認証情報に誤りがあります。",
+			})
+			return
+		}
+
+		// 認証成功の場合
+		c.Redirect(http.StatusFound, "/index")
 	})
 
 	// *************************************
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24
 require github.com/magefile/mage v1.15.0
 
 require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
@@ -12,10 +13,13 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v1.0.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.7 // indirect
+	github.com/go-ldap/ldap/v3 v3.4.10 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
